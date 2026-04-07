aeb74fdc7e0313bf37ecc858abb25f407dcaa1da
olha-d-d-00 <olgadema39@gmail.com>
2026-04-08 02:07:13 +0300

    Mongo

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
+import datetime
 import json
 import random
-from tempfile import template
-from typing import Annotated
+import pymongo
 
 import aiofiles
 from fastapi import FastAPI, Request, Form
@@ -15,6 +15,9 @@ app = FastAPI()
 
 templates = Jinja2Templates(directory="templates")
 
+client = pymongo.AsyncMongoClient("mongodb://admin:password@localhost:27017/")
+shortener_db = client["shortener_db"]
+urls_collection = shortener_db["urls"]
 @app.get("/")
 async def root(request: Request):
     return templates.TemplateResponse(
@@ -28,26 +31,28 @@ async def create_short_url(long_url: Annotated[str, Form()]):
 
     available_characters = string.ascii_letters + string.digits
 
-    async with aiofiles.open("urls.json", "r") as f:
-        final_urls_dict = json.loads(await f.read())
+    # async with aiofiles.open("urls.json", "r") as f:
+    #     final_urls_dict = json.loads(await f.read())
 
     short_url = "".join([random.choice(available_characters) for _ in range(6+1)])
-    while short_url in final_urls_dict:
+    while await urls_collection.find_one({"short_url": short_url}) is not None:
         short_url = "".join([random.choice(available_characters) for _ in range(6+1)])
 
-    final_urls_dict[short_url] = long_url
+    final_document = {"short_url": short_url, "long_url": long_url, "counter_clicks": 0, "created_at": datetime.datetime.now()}
 
-    async with aiofiles.open("urls.json", "w") as f:
-        await f.write(json.dumps(final_urls_dict))
+    await urls_collection.insert_one(final_document)
+
 
     return {"message": f"Your original URL: {long_url}, Short_url: http://127.0.0.1:8000/{short_url}"}
 
+
+
 @app.get("/{short_url}")
 async def url_redirect(short_url: str):
-    async with aiofiles.open("urls.json", "r") as f:
-        final_urls_dict = json.loads(await f.read())
-
-    final_url = final_urls_dict.get(short_url)
-    if final_url:
+    final_urls_dict = await urls_collection.find_one({"short_url": short_url})
+    if final_urls_dict:
+        query_filter = {"short_url": short_url}
+        await urls_collection.update_one(query_filter, {"$inc": {"counter_clicks": 1}})
+        final_url = final_urls_dict.get("long_url")
         return RedirectResponse(final_url)
     return {"message": f"Error, no URL found for {short_url}"}
